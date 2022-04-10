1048efbaa70a8044aba31edd0833c892e73dbe27
Luis Boavida <rodriguesl@coventry.ac.uk>
2022-04-10 11:32:57 +0000

    improved comments of feature-3

--- public/js/availableParcels.js
+++ public/js/availableParcels.js
@@ -1,4 +1,4 @@
-//These two functions gets and returns the user location
+//This function gets and returns the user location
 //https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_geolocation
 function getUserCoordinatesPromise() {
   return new Promise(function(resolve, reject) {
@@ -8,8 +8,8 @@ function getUserCoordinatesPromise() {
 
 async function getUserCoordinates() {
   try {
-    return await getUserCoordinatesPromise()
-  } catch (err) {
+    return await getUserCoordinatesPromise()  //Tries to get the user coordinates
+  } catch (err) { //If error then user didnt accepted location
     alert("You didn't allowed your location, so the parcels will not show in order of distance") 
     return false
   }
@@ -18,28 +18,22 @@ async function getUserCoordinates() {
 //Get available parcels
 async function getAvailableParcels(userLocation) { 
   let response = ''
-  if (userLocation == false)
+  if (userLocation == false)  //If location not enabled, then get parcels by date
     response = await fetch(`/getAvailableParcelsApi/`)
-  else 
-    response = await fetch(`/getAvailableParcelsApi/${userLocation.coords.latitude}/${userLocation.coords.longitude}`)    //fetches the parcels
+  else //If location enabled, then get parcels by distance
+    response = await fetch(`/getAvailableParcelsApi/${userLocation.coords.latitude}/${userLocation.coords.longitude}`)
 
-  let data = await response.json()
+  let data = await response.json()  //Converts the response into a usable object
   return data   //returns it
 }
 
-//NOT NEEDED I THIKN
-async function getCoordinates(address) { 
-  let response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAcVD7SuDFqDU6J7OQtZl-T87HIzR74wR4&address=' + address)    //fetches the address
-  let data = await response.json()
-  return data.results[0].geometry.location   //returns it
-}
-
-function showParcels(parcels) {
-  if (!parcels.length) document.getElementsByTagName('main')[0].innerHTML += `<h1>No parcels available</h1>`
+function showParcels(parcels) { //Writes the html of each parcel
+  if (!parcels.length) document.getElementsByTagName('main')[0].innerHTML += `<h1>No parcels available</h1>`  //If there is no parcels, show message
 
   for (parcel of parcels) {
-    const hoursSinceStarted = 'Hours since added: ' + Math.floor(Math.abs(new Date() - new Date(parcel.dateAndTimeAdded)) / 36e5)
+    const hoursSinceStarted = 'Hours since added: ' + Math.floor(Math.abs(new Date() - new Date(parcel.dateAndTimeAdded)) / 36e5) //Get the hours since added
 
+    //Writes the html of each parcel
     document.getElementsByTagName('main')[0].innerHTML += `
       <section>
         <h2>${parcel.parcelName}</h2>
@@ -55,8 +49,8 @@ function showParcels(parcels) {
   }
 }
 
-window.addEventListener('load', async function() {
-    const userLocation = await getUserCoordinates()
-    const parcels = await getAvailableParcels(userLocation)
-    showParcels(parcels)
+window.addEventListener('load', async function() {  //On load function
+    const userLocation = await getUserCoordinates() //Get the coordinates from user
+    const parcels = await getAvailableParcels(userLocation) //Get the available parcels
+    showParcels(parcels)  //Writes the parcels
 }, false)
